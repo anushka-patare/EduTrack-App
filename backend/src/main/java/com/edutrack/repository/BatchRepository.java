91470a9885c71356641b06d334361557a2abfe6e
Anushka Patare <anupatare07@gmail.com>
2026-04-15 16:57:43 +0530

    Delete backend directory

--- backend/src/main/java/com/edutrack/repository/BatchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edutrack.repository;
-
-import com.edutrack.model.Batch;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BatchRepository extends MongoRepository<Batch, String> {
-}
--- backend/src/main/java/com/edutrack/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edutrack.repository;
-
-import com.edutrack.model.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface StudentRepository extends MongoRepository<Student, String> {
-    List<Student> findByBatch(String batch);
-}
